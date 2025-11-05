24b23a151e41f48a9e763eb0aa07fc6f3eac0efd
Narasimha <lakshminarasimh44@gmail.com>
2025-11-06 03:59:08 +0530

    Fixed Nodemailer and inline email template6

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,7 @@ mongoose
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   methods: ["GET","POST","PUT","DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
